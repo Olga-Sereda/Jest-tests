8b077667acde2c52ef69ba0617fbf772d8aafeb1
Olga <olya.sereda.97@mail.ru>
2024-05-03 19:55:03 +0300

    update functions and add tests

--- __tests__/card.test.ts
+++ __tests__/card.test.ts
@@ -1,11 +1,108 @@
-import '../src/main.js'
+import {
+  isCardValid,
+  isCardValidMaxLength,
+  isCardValidMinLength,
+  isCardValidNumber,
+  isEmailValid,
+  isEmailStart,
+  isEmailStartNumb,
+  isEmailValidIncl,
+  isDateValid,
+  isCVVValid,
+  isCVVValidMaxLength,
+  isCVVValidMinLength,
+  isCVVValidNumb
+} from '../src/main.js'
 
 describe('CardNumber', () => {
-  it('Недостаточное кол-во цифр', () => {
-    const result = document.getElementById('cardNumber')
-    result.focus()
-    result.textContent = '1234'
-    result.blur()
-    expect(result.classList.contains('invalid')).toBe(true)
+  // it('Недостаточное кол-во цифр', () => {
+  //   const result = document.getElementById('cardNumber')
+  //   result.focus()
+  //   result.textContent = '1234'
+  //   result.blur()
+  //   expect(result.classList.contains('invalid')).toBe(true)
+  // })
+  it('В номере карты верное количество цифр', () => {
+    const result = isCardValid('1234567890123456')
+
+    expect(result).toBeTruthy()
+  })
+
+  it('Недостаточное кол-во цифр в номере карты', () => {
+    const result = isCardValidMinLength('1234')
+
+    expect(result).toBeTruthy()
+  })
+
+  it('Слишком большое кол-во цифр в номере карты', () => {
+    const result = isCardValidMaxLength('123456789012345678')
+
+    expect(result).toBeTruthy()
+  })
+
+  it('В номере карты только цифры', () => {
+    const result = isCardValidNumber('sc!%/d4')
+
+    expect(result).toBeFalsy()
+  })
+})
+
+describe('email', () => {
+  it('Почта введена корректно', () => {
+    const result = isEmailValid('mailname@domen.ru')
+
+    expect(result).toBeTruthy()
+  })
+
+  it('Почта не начинается с @', () => {
+    const result = isEmailStart('@domen.ru')
+
+    expect(result).toBeTruthy()
+  })
+
+  it('Почта не содержит @', () => {
+    const result = isEmailValidIncl('mailomen.ru')
+
+    expect(result).toBeFalsy()
+  })
+
+  it('Почта не начинается с цифр', () => {
+    const result = isEmailStartNumb('123me@domen.ru')
+
+    expect(result).toBeTruthy()
+  })
+})
+
+describe('date', () => {
+  it('Дата должна содержать не больше 4 цифр', () => {
+    const result = isDateValid('122412')
+
+    expect(result).toBeFalsy()
+  })
+})
+
+describe('CVV', () => {
+  it('CVV должен содержать 3 цифры', () => {
+    const result = isCVVValid('666')
+
+    expect(result).toBeTruthy()
+  })
+
+  it('CVV должен содержать не больше 3 цифр', () => {
+    const result = isCVVValidMaxLength('66654')
+
+    expect(result).toBeTruthy()
+  })
+
+  it('CVV должен содержать не меньше 3 цифр', () => {
+    const result = isCVVValidMinLength('66')
+
+    expect(result).toBeTruthy()
+  })
+
+  it('CVV должен содержать цифры', () => {
+    const result = isCVVValidNumb('/d4')
+
+    expect(result).toBeFalsy()
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { el, mount, setAttr } from 'redom'
-import Cleave from 'cleave.js'
+// import Cleave from 'cleave.js'
 
 const label = el('label.labelCard#labelId', 'Введите данные карты')
-export const input = el('input.cartNumber#cardNumber', {
+const input = el('input.cartNumber#cardNumber', {
   placeholder: '0000-0000-0000-0000',
-  required: 'required'
+  required: 'required',
+  type: 'number'
 })
 const errorCard = el('.error#errorId', { style: { color: 'red' } })
 
@@ -45,20 +46,26 @@ const container = el(
 )
 mount(document.body, container)
 
-inputMail.addEventListener('blur', () => {
-  // @ts-expect-error Property 'value' does not exist on type 'HTMLElement'.ts(2339)
-  if (!inputMail.value.includes('@')) {
-    inputMail.classList.add('invalid')
-    error.textContent = 'Пожалуйста, введите правильный email.'
-  } else {
-    inputMail.classList.remove('invalid')
-    error.textContent = ''
-  }
-})
+//проверка карты
+export const isCardValidMaxLength = (card: string): boolean => {
+  return card.length >= 17
+}
+
+export const isCardValidMinLength = (card: string): boolean => {
+  return card.length <= 11
+}
+
+export const isCardValidNumber = (card: string): boolean => {
+  return /^\d+$/.test(card)
+}
+
+export const isCardValid = (card: string): boolean => {
+  return card.length === 16
+}
 
 input.addEventListener('blur', () => {
-  // @ts-expect-error Property 'value' does not exist on type 'HTMLElement'.ts(2339)
-  if (input.value.length <= 15) {
+  //  @ts-expect-error Property 'value' does not exist on type 'HTMLElement'.ts(2339)
+  if (!isCardValid(input.value) || isCardValidMinLength(input.value)) {
     input.classList.add('invalid')
     errorCard.innerHTML = 'Пожалуйста, введите корректный номер'
     return false
@@ -69,9 +76,27 @@ input.addEventListener('blur', () => {
   }
 })
 
+input.addEventListener('blur', () => {
+  //  @ts-expect-error Property 'value' does not exist on type 'HTMLElement'.ts(2339)
+  if (isCardValidMaxLength(input.value) || !isCardValidNumber(input.value)) {
+    input.classList.add('invalid')
+    errorCard.innerHTML = 'Пожалуйста, введите корректный номер'
+    return false
+  } else {
+    input.classList.remove('invalid')
+    errorCard.innerHTML = ''
+    return true
+  }
+})
+
+//валидация даты
+export const isDateValid = (date: string): boolean => {
+  return date.length < 4
+}
+
 inputDate.addEventListener('blur', () => {
   // @ts-expect-error Property 'value' does not exist on type 'HTMLElement'.ts(2339)
-  if (inputDate.value.length < 4) {
+  if (!isDateValid(inputDate.value)) {
     inputDate.classList.add('invalid')
     errorDate.innerHTML = 'Пожалуйста, введите корректную дату'
   } else {
@@ -80,14 +105,85 @@ inputDate.addEventListener('blur', () => {
   }
 })
 
+//проверка CVV
+export const isCVVValid = (cvv: string): boolean => {
+  return cvv.length === 3
+}
+
+export const isCVVValidMinLength = (cvv: string): boolean => {
+  return cvv.length < 3
+}
+
+export const isCVVValidMaxLength = (cvv: string): boolean => {
+  return cvv.length > 3
+}
+
+export const isCVVValidNumb = (cvv: string): boolean => {
+  return /^\d+$/.test(cvv)
+}
+
 inputCVV.addEventListener('blur', () => {
   // @ts-expect-error Property 'value' does not exist on type 'HTMLElement'.ts(2339)
-  if (inputCVV.value.length < 3) {
+  if (!isCVVValid(inputCVV.value) || isCVVValidMaxLength(inputCVV.value)) {
     inputCVV.classList.add('invalid')
     errorCVV.textContent = 'Пожалуйста, введите корректный CVV'
   } else {
     inputCVV.classList.remove('invalid')
     errorCVV.textContent = ''
+  }
+})
+
+inputCVV.addEventListener('blur', () => {
+  // @ts-expect-error Property 'value' does not exist on type 'HTMLElement'.ts(2339)
+  if (isCVVValidMinLength(inputCVV.value) || !isCVVValidNumb(inputCVV.value)) {
+    inputCVV.classList.add('invalid')
+    errorCVV.textContent = 'Пожалуйста, введите корректный CVV'
+  } else {
+    inputCVV.classList.remove('invalid')
+    errorCVV.textContent = ''
+  }
+})
+
+//проверка почты
+export const isEmailValid = (email: string): boolean => {
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
+  return emailRegex.test(email)
+}
+
+export const isEmailValidIncl = (email: string): boolean => {
+  return email.includes('@')
+}
+
+export const isEmailStart = (email: string): boolean => {
+  return email.startsWith('@')
+}
+
+export const isEmailStartNumb = (email: string): boolean => {
+  const numbers = email.match(/^\d+/)[0]
+  return email.startsWith(numbers)
+}
+
+inputMail.addEventListener('blur', () => {
+  //  @ts-expect-error Property 'value' does not exist on type 'HTMLElement'.ts(2339)
+  if (!isEmailValid(inputMail.value) || !isEmailValidIncl(inputMail.value)) {
+    inputMail.classList.add('invalid')
+    error.textContent = 'Пожалуйста, введите правильный email.'
+  } else {
+    inputMail.classList.remove('invalid')
+    error.textContent = ''
+  }
+})
+
+inputMail.addEventListener('blur', () => {
+  //  @ts-expect-error Property 'value' does not exist on type 'HTMLElement'.ts(2339)
+  if (isEmailStart(inputMail.value) || isEmailStartNumb(inputMail.value)) {
+    inputMail.classList.add('invalid')
+    error.textContent = 'Пожалуйста, введите правильный email.'
+  } else {
+    inputMail.classList.remove('invalid')
+    error.textContent = ''
   }
 })
 
@@ -115,16 +211,16 @@ for (const inputVal of inputs) {
   })
 }
 
-new Cleave(input, {
-  creditCard: true,
-  creditCardStrictMode: true
-})
+// new Cleave(input, {
+//   creditCard: true,
+//   creditCardStrictMode: true
+// })
 
-new Cleave(inputDate, {
-  date: true,
-  datePattern: ['m', 'y']
-})
+// new Cleave(inputDate, {
+//   date: true,
+//   datePattern: ['m', 'y']
+// })
 
-new Cleave(inputCVV, {
-  blocks: [3]
-})
+// new Cleave(inputCVV, {
+//   blocks: [3]
+// })
